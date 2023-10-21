417301c6de851d08a63f7aecc34ffb0d39e979eb
ziorye <ziorye@gmail.com>
2023-10-21 12:59:39 +0800

    test: sout

--- sb-01-quickstart/src/main/java/com/ziorye/MyApplication.java
+++ sb-01-quickstart/src/main/java/com/ziorye/MyApplication.java
@@ -2,12 +2,23 @@ package com.ziorye;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class MyApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MyApplication.class, args);
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(MyApplication.class, args);
+
+        System.out.println(Arrays.toString(applicationContext.getBeanDefinitionNames()));
+
+        boolean containsMyApplicationBean = applicationContext.containsBean("myApplication");
+        System.out.println(containsMyApplicationBean);
+
+        boolean containsDispatcherServletBean = applicationContext.containsBean("dispatcherServlet");
+        System.out.println(containsDispatcherServletBean);
     }
 
 }
