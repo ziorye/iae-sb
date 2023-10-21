9663832a348d2e30a06553c0378ee2dcac037e70
ziorye <ziorye@gmail.com>
2023-10-21 16:11:10 +0800

    @EnableConfigurationProperties + @ConfigurationProperties

---
+++ sb-01-quickstart/src/main/java/com/ziorye/config/MyConfig.java
@@ -0,0 +1,9 @@
+package com.ziorye.config;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(MyProperties.class)
+public class MyConfig {
+}
