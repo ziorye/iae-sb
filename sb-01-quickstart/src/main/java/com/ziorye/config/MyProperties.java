2084355df766f049c6cbfe5acdfe026379ab8244
ziorye <ziorye@gmail.com>
2023-10-21 16:44:59 +0800

    lombok
    https://projectlombok.org/

--- sb-01-quickstart/src/main/java/com/ziorye/config/MyProperties.java
+++ sb-01-quickstart/src/main/java/com/ziorye/config/MyProperties.java
@@ -1,41 +1,11 @@
 package com.ziorye.config;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "my")
+@Data
 public class MyProperties {
     private String name;
     private int age;
-
-    public MyProperties() {
-    }
-
-    public MyProperties(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return "MyProperties{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                '}';
-    }
 }
