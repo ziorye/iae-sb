852c9c3105862ef44b30cdb247eca5cfe4b0794c
ziorye <ziorye@gmail.com>
2023-10-21 20:54:14 +0800

    New module using Spring Initializr

---
+++ sb-02-web/src/main/java/com/ziorye/sb02web/Sb02WebApplication.java
@@ -0,0 +1,13 @@
+package com.ziorye.sb02web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb02WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb02WebApplication.class, args);
+	}
+
+}
---
+++ sb-02-web/src/test/java/com/ziorye/sb02web/Sb02WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ziorye.sb02web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb02WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
