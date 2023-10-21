5143560c78855e7221fbeefef87beb5da8f0b144
ziorye <ziorye@gmail.com>
2023-10-21 15:39:58 +0800

    test: @AutoConfigureMockMvc

---
+++ sb-01-quickstart/src/test/java/com/ziorye/controller/HelloControllerTest.java
@@ -0,0 +1,23 @@
+package com.ziorye.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class HelloControllerTest {
+
+    @Test
+    void home(@Autowired MockMvc mvc) throws Exception {
+        mvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello World!"));
+    }
+}
