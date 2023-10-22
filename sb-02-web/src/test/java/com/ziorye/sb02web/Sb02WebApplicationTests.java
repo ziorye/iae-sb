83fac34d843656d88143d545cbaa51804a8634c3
ziorye <ziorye@gmail.com>
2023-10-22 11:47:58 +0800

    Static Content
    https://docs.spring.io/spring-boot/docs/current/reference/html/web.html#web.servlet.spring-mvc.static-content

--- sb-02-web/src/test/java/com/ziorye/sb02web/Sb02WebApplicationTests.java
+++ sb-02-web/src/test/java/com/ziorye/sb02web/Sb02WebApplicationTests.java
@@ -1,6 +1,7 @@
 package com.ziorye.sb02web;
 
 import org.hamcrest.core.StringContains;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -17,8 +18,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 class Sb02WebApplicationTests {
 
+	@Autowired MockMvc mvc;
+
 	@Test
-	void screenshot(@Autowired MockMvc mvc) throws Exception {
+	void screenshot() throws Exception {
 		for (String img : Arrays.asList(
 				"New Module using Spring Initializr - 1.png",
 				"New Module using Spring Initializr - 2 - select dependencies.png")
@@ -30,17 +33,22 @@ class Sb02WebApplicationTests {
 	}
 
 	@Test
-	void index(@Autowired MockMvc mvc) throws Exception {
+	void index() throws Exception {
 		mvc.perform(get("/index.html"))
 				.andExpect(status().isOk())
 				.andExpect(content().string(StringContains.containsString("New module using Spring Initializr")));
 	}
 
 	@Test
-	void autoconfigure(@Autowired MockMvc mvc) throws Exception {
+	void autoconfigure() throws Exception {
 		mvc.perform(get("/autoconfigure.html"))
 				.andExpect(status().isOk())
 				.andExpect(content().string(StringContains.containsString("Auto Configuration")));
 	}
 
+	@Test
+	@DisplayName("测试静态资源")
+	void testStaticContent() throws Exception {
+		mvc.perform(get("/img/default-locations-of-static-resources.png")).andExpect(content().contentType("image/png"));
+	}
 }
