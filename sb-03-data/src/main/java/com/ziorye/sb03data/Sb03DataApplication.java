a3e50ca11541de107386f977238d23931730ddf3
ziorye <ziorye@gmail.com>
2023-10-23 14:18:41 +0800

    New sb-03-data module using Spring Initializr

---
+++ sb-03-data/src/main/java/com/ziorye/sb03data/Sb03DataApplication.java
@@ -0,0 +1,13 @@
+package com.ziorye.sb03data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb03DataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb03DataApplication.class, args);
+	}
+
+}
---
+++ sb-03-data/src/test/java/com/ziorye/sb03data/Sb03DataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ziorye.sb03data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb03DataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
