4a4fc69a8813516cdd25bb8d8804dc639be95857
ziorye <ziorye@gmail.com>
2023-10-24 07:32:17 +0800

    New sb-06-doc module using Spring Initializr

---
+++ sb-06-doc/src/main/java/com/ioewvsau/Sb06DocApplication.java
@@ -0,0 +1,13 @@
+package com.ioewvsau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb06DocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb06DocApplication.class, args);
+    }
+
+}
---
+++ sb-06-doc/src/test/java/com/ioewvsau/Sb06DocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ioewvsau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb06DocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
