0721ccbbca6a661ccf5f7cde48b13feb960561c4
ziorye <ziorye@gmail.com>
2023-10-23 15:04:23 +0800

    combine schema and data

--- sb-03-data/src/main/resources/data.sql
+++ sb-03-data/src/main/resources/data.sql
@@ -1,2 +1,15 @@
+INSERT INTO user (id, name, age, email, password)
+VALUES (1, 'test1', 18, 'test1@test.com', 'secret'),
+       (2, 'test2', 19, 'test2@test.com', 'secret'),
+       (3, 'test3', 20, 'test3@test.com', 'secret'),
+       (4, 'test4', 21, 'test4@test.com', 'secret'),
+       (5, 'test5', 22, 'test5@test.com', 'secret'),
+       (6, 'test6', 23, 'test6@test.com', 'secret'),
+       (7, 'test7', 24, 'test7@test.com', 'secret'),
+       (8, 'test8', 25, 'test8@test.com', 'secret'),
+       (9, 'test9', 26, 'test9@test.com', 'secret'),
+       (10, 'test10', 27, 'test10@test.com', 'secret');
+
+
 INSERT INTO `city` (`id`, `name`, `state`, `country`)
 VALUES (1, 'San Francisco', 'CA', 'US');
--- sb-03-data/src/main/resources/schema.sql
+++ sb-03-data/src/main/resources/schema.sql
@@ -1,3 +1,15 @@
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`
+(
+    `id`       int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+    `name`     varchar(30) NOT NULL DEFAULT '' COMMENT '姓名',
+    `age`      int(11) DEFAULT NULL COMMENT '年龄',
+    `email`    varchar(100)         DEFAULT NULL COMMENT '邮箱',
+    `password` varchar(100)         DEFAULT NULL COMMENT '密码',
+    PRIMARY KEY (`id`)
+);
+
+
 DROP TABLE IF EXISTS `city`;
 CREATE TABLE `city`
 (
