a22480a335b70be01e3107ca951578721a74ab42
ziorye <ziorye@gmail.com>
2023-10-24 09:54:06 +0800

    template.setDefaultSerializer

--- sb-05-redis/src/main/java/com/ziorye/sb05redis/bean/User.java
+++ sb-05-redis/src/main/java/com/ziorye/sb05redis/bean/User.java
@@ -2,11 +2,13 @@ package com.ziorye.sb05redis.bean;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class User implements Serializable {
     private Long id;
     private String name;
