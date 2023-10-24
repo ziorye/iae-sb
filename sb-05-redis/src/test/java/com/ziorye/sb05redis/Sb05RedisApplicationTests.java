b5c4dc319ec074aa02272d899c7111024c0ded0f
ziorye <ziorye@gmail.com>
2023-10-24 09:54:05 +0800

    New sb-05-redis module using Spring Initializr

---
+++ sb-05-redis/src/test/java/com/ziorye/sb05redis/Sb05RedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ziorye.sb05redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb05RedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
