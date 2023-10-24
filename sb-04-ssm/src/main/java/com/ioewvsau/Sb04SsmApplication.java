1179244c568989d6e4d86712b79ffb95528d1f06
ziorye <ziorye@gmail.com>
2023-10-24 09:54:05 +0800

    New sb-04-ssm module using Spring Initializr

---
+++ sb-04-ssm/src/main/java/com/ioewvsau/Sb04SsmApplication.java
@@ -0,0 +1,13 @@
+package com.ioewvsau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb04SsmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb04SsmApplication.class, args);
+    }
+
+}
---
+++ sb-04-ssm/src/test/java/com/ioewvsau/Sb04SsmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ioewvsau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb04SsmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
