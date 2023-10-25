077a66c21635d0b88dafb38e755c96e8aa2ddfdc
ziorye <ziorye@gmail.com>
2023-10-25 17:10:24 +0800

    New sb-07-actuator module using Spring Initializr

---
+++ sb-07-actuator/src/main/java/com/ziorye/sb07actuator/Sb07ActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.ziorye.sb07actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb07ActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb07ActuatorApplication.class, args);
+	}
+
+}
