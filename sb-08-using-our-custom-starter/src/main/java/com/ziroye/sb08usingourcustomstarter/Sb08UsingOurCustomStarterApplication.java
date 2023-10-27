7557b487aebfc2bd3b455e0ad5d75d43dafb562c
ziorye <ziorye@gmail.com>
2023-10-27 15:23:23 +0800

    New sb-08-***-starter module using Spring Initializr

---
+++ sb-08-using-our-custom-starter/src/main/java/com/ziroye/sb08usingourcustomstarter/Sb08UsingOurCustomStarterApplication.java
@@ -0,0 +1,13 @@
+package com.ziroye.sb08usingourcustomstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb08UsingOurCustomStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb08UsingOurCustomStarterApplication.class, args);
+    }
+
+}
---
+++ sb-08-using-our-custom-starter/src/test/java/com/ziroye/sb08usingourcustomstarter/Sb08UsingOurCustomStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ziroye.sb08usingourcustomstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb08UsingOurCustomStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
