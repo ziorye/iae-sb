3956e7ffd75fbb001e509f03bd6012eebee05c7f
ziorye <ziorye@gmail.com>
2023-11-17 11:32:58 +0800

    @CrossOrigin
    https://docs.spring.io/spring-framework/reference/web/webmvc-cors.html#mvc-cors-controller

--- sb-06-doc/src/main/java/com/ioewvsau/controller/PostController.java
+++ sb-06-doc/src/main/java/com/ioewvsau/controller/PostController.java
@@ -17,6 +17,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/posts")
 @Tag(name = "post-controller", description = "文章管理")
+@CrossOrigin
 public class PostController {
     @Autowired
     PostService postService;
